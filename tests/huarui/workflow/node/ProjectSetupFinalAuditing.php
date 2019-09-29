5a45c66b8966658048ce8036cec10f0baf8da722
FizeChan <chenfengzhan@qq.com>
2019-09-29 16:30:27 +0800

    代码风格优化

---
+++ tests/huarui/workflow/node/ProjectSetupFinalAuditing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cfz87
+ * Date: 2018-08-13
+ * Time: 16:42
+ */
+
+namespace huarui\workflow\node;
+
+
+class ProjectSetupFinalAuditing
+{
+
+}
---
+++ tests/huarui/workflow/node/ProjectSetupRetrialAuditing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cfz87
+ * Date: 2018-08-13
+ * Time: 16:41
+ */
+
+namespace huarui\workflow\node;
+
+
+class ProjectSetupRetrialAuditing
+{
+
+}
